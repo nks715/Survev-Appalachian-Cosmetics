33d8b8d7945e0558574099892711920ca0f4e6f7
nks715 <neelsreenivasa@gmail.com>
2026-08-24 16:57:00 -0700

    Update and rename n7t0w4Al.js to helmetchanger.js

---
+++ helmetchanger.js
@@ -0,0 +1,60 @@
+    helmet01: {
+        name: `Level 1 Helmet`,
+        type: `helmet`,
+        level: 1,
+        damageReduction: .25,
+        skinImg: {
+            baseTint: 0xffffff,
+            baseSprite: `cowboy-hat-03.img`
+        },
+        lootImg: {
+            sprite: `loot-helmet-01.img`,
+            tint: 16777215,
+            border: `loot-circle-outer-01.img`,
+            borderTint: 0,
+            scale: .2
+        },
+        sound: {
+            pickup: `helmet_pickup_01`
+        }
+    },
+    helmet02: {
+        name: `Level 2 Helmet`,
+        type: `helmet`,
+        level: 2,
+        damageReduction: .4,
+        skinImg: {
+            baseTint: 0xffffff,
+            baseSprite: `cowboy-hat-02.img`
+        },
+        lootImg: {
+            sprite: `loot-helmet-02.img`,
+            tint: 16777215,
+            border: `loot-circle-outer-01.img`,
+            borderTint: 0,
+            scale: .2
+        },
+        sound: {
+            pickup: `helmet_pickup_01`
+        }
+    },
+    helmet03: {
+        name: `Level 3 Helmet`,
+        type: `helmet`,
+        level: 3,
+        damageReduction: .55,
+        skinImg: {
+            baseTint: 0xffffff,
+            baseSprite: `cowboy-hat.img`
+        },
+        lootImg: {
+            sprite: `loot-helmet-03.img`,
+            tint: 16777215,
+            border: `loot-circle-outer-01.img`,
+            borderTint: 0,
+            scale: .2
+        },
+        sound: {
+            pickup: `helmet_pickup_01`
+        }
+    },
