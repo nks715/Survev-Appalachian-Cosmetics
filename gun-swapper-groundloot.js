d71a2816f541f783a13ee262fdde119828297114
nks715 <neelsreenivasa@gmail.com>
2026-08-30 17:43:25 -0700

    Create gun-swapper-groundloot.js

---
+++ gun-swapper-groundloot.js
@@ -0,0 +1,23 @@
+            // m870 Test Injection
+            if (e === `loadout`) {
+                const baseTexture = r.baseTexture;
+                const existingTexture = r.textures[`loot-weapon-m870.img`];
+            
+                if (baseTexture && existingTexture) {
+                    const TextureClass = existingTexture.constructor;
+                    const RectangleClass = existingTexture.frame.constructor;
+            
+                    const cowboyHatTexture = new TextureClass(
+                        baseTexture,
+                        new RectangleClass(3500, 3000, 128, 128)
+                    );
+            
+                    TextureClass.addToCache(cowboyHatTexture, `loot-weapon-m870.img`);
+            
+                    r.textures[`loot-weapon-m870.img`] = cowboyHatTexture;
+                    console.log(
+                        `[Guns] Registered old-loot-weapon-m870.img`,
+                        cowboyHatTexture
+                    );
+                }
+            }
