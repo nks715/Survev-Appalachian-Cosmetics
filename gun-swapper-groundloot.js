0e643fec8be7eaec7273663ed9a85c5f932a220c
nks715 <neelsreenivasa@gmail.com>
2026-09-04 21:57:57 -0700

    Implement custom texture loading for scout elite
    Added support for loading custom texture for scout elite.

--- gun-swapper-groundloot.js
+++ gun-swapper-groundloot.js
@@ -21,3 +21,26 @@
                     );
                 }
             }
+            // scout elite to HLR 223 test injection
+            if (e === `loadout`) {
+                const baseTexture = r.baseTexture;
+                const existingTexture = r.textures[`loot-weapon-scout.img`];
+            
+                if (baseTexture && existingTexture) {
+                    const TextureClass = existingTexture.constructor;
+                    const RectangleClass = existingTexture.frame.constructor;
+            
+                    const cowboyHatTexture = new TextureClass(
+                        baseTexture,
+                        new RectangleClass(3505, 3150, 128, 128)
+                    );
+            
+                    TextureClass.addToCache(cowboyHatTexture, `loot-weapon-scout.img`);
+            
+                    r.textures[`loot-weapon-scout.img`] = cowboyHatTexture;
+                    console.log(
+                        `[Guns] Registered loot-weapon-hlr.img`,
+                        cowboyHatTexture
+                    );
+                }
+            }
